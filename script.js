54beb20fe327d583401cb6b1dfa2ce6fa3b3a07e
Siya <tonjenisiya@gmail.com>
2024-06-04 21:18:12 +0200

    Added Loader and Few adjustments

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const splash = document.querySelector('.splash');
 document.addEventListener('DOMContentLoaded', (e)=>{
     setTimeout(() => {
         splash.classList.add('display-none');
-    }, 4000);
-})
+    }, 6000);
+});
+
 
 // Roll the dice function
 function rollDice() {
